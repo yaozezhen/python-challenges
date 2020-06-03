8bcac45e2e1285355d8c2bf36e1abcbe52c2b23b
jayala-29 <jayala@ucsd.edu>
2020-06-02 22:47:07 -0700

    Create mostCommon.py

---
+++ mostCommon.py
@@ -0,0 +1,7 @@
+# function descriptor: mostCommon(ls)
+# finds the word(s) that shows up the most in a list of strings
+# if two or more words show up the same amount of times, return all of them
+# ls - list of strings
+# return type: list of strings
+def mostCommon(ls):
+  # fill-in code here
