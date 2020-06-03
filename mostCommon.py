ab2199ea3899ce969174cec37645ed983c0eaaca
jayala-29 <jayala@ucsd.edu>
2020-06-02 22:48:44 -0700

    Update mostCommon.py

--- mostCommon.py
+++ mostCommon.py
@@ -3,5 +3,6 @@
 # if two or more words show up the same amount of times, return all of them
 # ls - list of strings
 # return type: list of strings
+# example: ls - ["hello", "hi", "hello"] => return ["hello"]
 def mostCommon(ls):
   # fill-in code here
