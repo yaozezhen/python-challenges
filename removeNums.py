973e649bc5f61a2199422b83cc8bf0ae7b6e9dc2
jayala-29 <jayala@ucsd.edu>
2020-06-02 22:51:34 -0700

    Update removeNums.py

--- removeNums.py
+++ removeNums.py
@@ -2,5 +2,6 @@
 # removes numbers from a given string
 # s - a string that only contains alphanumeric characters
 # return type: string
+# example: s - "Hel1o" => return "Helo"
 def removeNums(s):
   # fill-in code here
