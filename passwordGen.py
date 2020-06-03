10afbc48e127effbfaf07ad995b3379f8b0061a0
jayala-29 <jayala@ucsd.edu>
2020-06-02 23:12:56 -0700

    Update passwordGen.py

--- passwordGen.py
+++ passwordGen.py
@@ -1,3 +1,4 @@
+import random
 # function descriptor: passwordGen(pl)
 # randomly generates a strong password including numbers, letters, and special characters
 # pl - password length (in characters), must be at least 12 and at most 16
