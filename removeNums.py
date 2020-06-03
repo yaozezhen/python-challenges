23512f166db5cf347c57c579c06ec02bb2268106
jayala-29 <jayala@ucsd.edu>
2020-06-02 22:36:08 -0700

    Create removeNums.py

---
+++ removeNums.py
@@ -0,0 +1,6 @@
+# function descriptor: removeNums(s)
+# removes numbers from a given string
+# s - a string that only contains alphanumeric characters
+# return type: string
+def removeNums(s):
+  # fill-in code here
