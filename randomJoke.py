3866e221494bc1e56a33915ec5b244bf8ee4c60c
jayala-29 <jayala@ucsd.edu>
2020-06-02 23:10:42 -0700

    Create randomJoke.py

---
+++ randomJoke.py
@@ -0,0 +1,12 @@
+import urllib.request, json 
+
+# function descriptor: randomJoke()
+# generates a random joke using the given URL containing a JSON object
+# your task is to figure out how JSON objects work since the joke is inside of it already, a hint is given below ;)
+# return type: string
+def randomJoke():
+  url = urllib.request.urlopen("https://api.icndb.com/jokes/random/")
+  data = json.loads(url.read())
+  # uncomment the line immediately below to see the contents of data, JSON objects are similar to dictionaries
+  # print (data)
+  # fill-in code
