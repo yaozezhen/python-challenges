9753a1090c87daca89e7c33d2f065a28b911424c
jayala-29 <jayala@ucsd.edu>
2020-06-02 22:38:24 -0700

    Create gcd.py

---
+++ gcd.py
@@ -0,0 +1,7 @@
+# function descriptor: gcd(m,n)
+# finds the greatest common divisor between two integers
+# m - first integer
+# n - second integer
+# return type: integer
+def gcd(m,n):
+  # fill-in code here
