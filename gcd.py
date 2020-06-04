94b92d0ae7351c1f2849ceed4ae285bbaef49b36
yaozezhen <63181901+yaozezhen@users.noreply.github.com>
2020-06-04 18:24:34 -0400

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -5,4 +5,23 @@
 # return type: integer
 # example: (m,n) - (12, 9) => return 3
 def gcd(m,n):
-  # fill-in code here
+  lsM = [m]
+  lsN = [n]
+  facM = 1
+  facN = 1
+  cd = []
+  while 2 * facM <= m: 
+    if (m % facM == 0):
+      lsM.append(facM)
+    facM += 1
+  while 2 * facN <= n: 
+    if (n % facN == 0): 
+      lsN.append(facN)
+    facN += 1
+  for dM in lsM: 
+    for dN in lsN: 
+      if (dM == dN): 
+        cd.append(dM)
+  return(max(cd))
+
+print(gcd(12,9))
