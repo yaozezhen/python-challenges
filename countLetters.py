27752b5b41e3ea302c9a9f32ca199455f27e88cc
jayala-29 <jayala@ucsd.edu>
2020-06-04 23:05:30 -0700

    Create countLetters.py

---
+++ countLetters.py
@@ -0,0 +1,16 @@
+# first play with what's below
+txt = input("Type something to test this out: ")
+print("Did you just type", txt)
+# delete the lines above once you understand how they work
+
+# function descriptor: countLetters(word)
+# keeps asking the user to count the letters in the word until they get it correct
+# word - any word that you want to user to count the number of letters in
+# return type: nothing; program should end when the user types the correct amount of letters
+# example: How many letters are in the word apple? 6
+#          Nope, try again! How many letters are in the word apple? 5
+#          Nice job! You got it :)
+def countLetters(word):
+  # fill-in code here
+  # leave the return statement below alone, feel free to delete this line
+  return 
