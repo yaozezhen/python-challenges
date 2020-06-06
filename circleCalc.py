540d3b3d115985bfb46f285d9b2a93dde7575118
jayala-29 <jayala@ucsd.edu>
2020-06-05 22:15:12 -0700

    Update circleCalc.py

--- circleCalc.py
+++ circleCalc.py
@@ -3,12 +3,12 @@ import math
 # calculates circle measures
 # r - positive number, radius
 # return type: nothing; program should end when the user types 'no'
-# example: What would you like to calculate? [area|circumference] area
+# example: What would you like to calculate? [area|circumference] 'area'
 #          Okay, the area is ___
-#          Do you want to calculate anything else? [y|n] y
-#          What would you like to calculate? [area|circumference] circumference
+#          Do you want to calculate anything else? [y|n] 'y'
+#          What would you like to calculate? [area|circumference] 'circumference'
 #          Okay, the circumference is ___
-#          Do you want to calculate anything else? [y|n] n
+#          Do you want to calculate anything else? [y|n] 'n'
 #          Okay, bye bye :)
 def circleCalc(r):
   # fill-in code here
