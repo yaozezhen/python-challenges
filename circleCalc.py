9b9befe97732a33e54f84bd1ea614e2868fdf0df
jayala-29 <jayala@ucsd.edu>
2020-06-05 22:14:41 -0700

    Create circleCalc.py

---
+++ circleCalc.py
@@ -0,0 +1,15 @@
+import math
+# function descriptor: circleCalc(r)
+# calculates circle measures
+# r - positive number, radius
+# return type: nothing; program should end when the user types 'no'
+# example: What would you like to calculate? [area|circumference] area
+#          Okay, the area is ___
+#          Do you want to calculate anything else? [y|n] y
+#          What would you like to calculate? [area|circumference] circumference
+#          Okay, the circumference is ___
+#          Do you want to calculate anything else? [y|n] n
+#          Okay, bye bye :)
+def circleCalc(r):
+  # fill-in code here
+  return 
