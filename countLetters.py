b21751a5ae952919baa84f18978f11de3fdc01e0
jayala-29 <jayala@ucsd.edu>
2020-06-05 22:15:51 -0700

    Update countLetters.py

--- countLetters.py
+++ countLetters.py
@@ -7,8 +7,8 @@ print("Did you just type", txt)
 # keeps asking the user to count the letters in the word until they get it correct
 # word - any word that you want to user to count the number of letters in
 # return type: nothing; program should end when the user types the correct amount of letters
-# example: How many letters are in the word apple? 6
-#          Nope, try again! How many letters are in the word apple? 5
+# example: How many letters are in the word apple? '6'
+#          Nope, try again! How many letters are in the word apple? '5'
 #          Nice job! You got it :)
 def countLetters(word):
   # fill-in code here
