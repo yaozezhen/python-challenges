5ffd0431d8212578b5a771b039b1c36a2c3a3aff
jayala-29 <jayala@ucsd.edu>
2020-06-06 22:17:10 -0700

    Create alphabetSort.py

---
+++ alphabetSort.py
@@ -0,0 +1,6 @@
+# function descriptor: alphabetSort(s)
+# sorts the letters in the string given
+# s - a string that only contains letters
+# return type: string
+# example: s - "happy" => return "ahppy"
+def alphabetSort(s):
