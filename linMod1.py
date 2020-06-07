aefeef97ba7f49ce68530f4fb22ba2ac28753450
jayala-29 <jayala@ucsd.edu>
2020-06-07 12:37:05 -0700

    Update linMod1.py

--- linMod1.py
+++ linMod1.py
@@ -4,6 +4,7 @@
 # b - integer constant on right side of equation
 # m - integer modulus number
 # conditions: no need to return multiple solutions, as long as it solves the equation
+#             should be a valid equation that has an integer solution (e.g. x is an integer in Z_m)
 # return type: integer
 # example: (a,b,m) - (3,7,4) => returns 1
 def linMod1(a,b,m):
