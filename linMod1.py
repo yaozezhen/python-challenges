8a1bddc71b44b2a0800a9f448a34065a466fead1
jayala-29 <jayala@ucsd.edu>
2020-06-07 12:35:36 -0700

    Create linMod1.py

---
+++ linMod1.py
@@ -0,0 +1,10 @@
+# function descriptor: linMod1(a,b,m)
+# solves linear modular equation of the form: ax (mod m) = b (mod m)
+# a - integer coefficient of x on left side of equation
+# b - integer constant on right side of equation
+# m - integer modulus number
+# conditions: no need to return multiple solutions, as long as it solves the equation
+# return type: integer
+# example: (a,b,m) - (3,7,4) => returns 1
+def linMod1(a,b,m):
+  # fill-in code here
