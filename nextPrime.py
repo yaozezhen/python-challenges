6f3733aaf3085dfe68f2b59d51af03da51276339
jayala-29 <jayala@ucsd.edu>
2020-06-06 22:42:48 -0700

    Create nextPrime.py

---
+++ nextPrime.py
@@ -0,0 +1,7 @@
+# function descriptor: nextPrime(n)
+# find the next prime number after the input value
+# n - positive integer
+# return type: integer
+# example: n - 12 => return 13
+def nextPrime(n):
+  # fill-in code here
