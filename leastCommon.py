0555b1d3ba004c48c3fbc25ab47fe99e12a34dd2
jayala-29 <jayala@ucsd.edu>
2020-06-07 22:25:07 -0700

    Create leastCommon.py

---
+++ leastCommon.py
@@ -0,0 +1,8 @@
+# function descriptor: leastCommon(ls)
+# finds the word(s) that shows up the least in a list of strings
+# if two or more words show up the same least amount of times, return all of them
+# ls - list of strings
+# return type: list of strings
+# example: ls - ["hello", "hi", "hello"] => return ["hi"]
+def leastCommon(ls): 
+  # fill-in code here
