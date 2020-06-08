9aecb9c2ef9e31a598eaad298b69f0b2fdd424cb
yaozezhen <63181901+yaozezhen@users.noreply.github.com>
2020-06-08 01:21:52 -0400

    Update nextPrime.py

--- nextPrime.py
+++ nextPrime.py
@@ -3,5 +3,20 @@
 # n - positive integer
 # return type: integer
 # example: n - 12 => return 13
+def checkPrime(n): 
+  p = 2
+  while p*p <= n: 
+    if (n % p == 0):
+      return False
+    p += 1
+  return True
+
 def nextPrime(n):
-  # fill-in code here
+  num = n+1
+  while True: 
+    if(checkPrime(num) == True): 
+      break
+    num += 1
+  return num
+
+print(nextPrime(13))
