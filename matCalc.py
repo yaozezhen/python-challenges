845d739fba8fb009f781203f607afa0603e46528
jayala-29 <jayala@ucsd.edu>
2020-06-08 16:32:49 -0700

    Create matCalc.py

---
+++ matCalc.py
@@ -0,0 +1,34 @@
+# function descriptors: matAdd(m1, m2) matSub(m1, m2) matMult(m1, m2)
+# perform respective operations on matricies after checking that dimensions are appropriate
+# m1 - matrix 1
+# m2 - matrix 2
+
+# matrix addition
+def matAdd(m1,m2):
+  # fill-in code here
+  
+# matrix subtraction
+def matSub(m1,m2):
+  # fill-in code here
+
+# matrix multiplication
+def matMult(m1,m2):
+  # fill-in code here
+  
+# create an identity matrix of dimensions n-by-n
+def matId(n):
+  # fill-in code here
+  
+# some sample matricies you can use to test out your functions
+sample1 = [[1,0],[0,1]]
+sample2 = [[1,2],[3,4]]
+sample3 = [[1,0,0],[0,1,0],[0,0,1]]
+sample4 = [[1],[2]]
+sample5 = [[0],[1],[2]]
+
+# sample test cases
+# matAdd(sample1,sample2)
+# matSub(sample1,sample2)
+# matMult(sample1,sample4)
+# matMult(sample3,sample5)
+# matId(4)
