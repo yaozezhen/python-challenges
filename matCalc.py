222cb0a90dad0ffeff344286d2f1fee6666e9796
yaozezhen <63181901+yaozezhen@users.noreply.github.com>
2020-06-09 01:55:50 -0400

    Update matCalc.py

--- matCalc.py
+++ matCalc.py
@@ -5,19 +5,57 @@
 
 # matrix addition
 def matAdd(m1,m2):
-  # fill-in code here
+  rows = len(m1)
+  cols = len(m1[0])
+  if(len(m2) != rows or len(m2[0]) != cols): 
+    return "Please double check the dimension of your matrices. "
+  output = [[0 for x in range(rows)] for y in range(cols)]
+  for i in range(rows): 
+    for j in range(cols): 
+      output[i][j] = m1[i][j] + m2[i][j]
+  return output
   
 # matrix subtraction
 def matSub(m1,m2):
-  # fill-in code here
+  rows = len(m1)
+  cols = len(m1[0])
+  if(len(m2) != rows or len(m2[0]) != cols): 
+    return "Please double check the dimension of your matrices. "
+  output = [[0 for x in range(rows)] for y in range(cols)]
+  for i in range(rows): 
+    for j in range(cols): 
+      output[i][j] = m1[i][j] - m2[i][j]
+  return output
 
 # matrix multiplication
 def matMult(m1,m2):
-  # fill-in code here
+  rows1 = len(m1)
+  cols1 = len(m1[0])
+  rows2 = len(m2)
+  cols2 = len(m2[0])
+  if(cols1 != rows2): 
+    return "Please double check the dimension of your matrices. "
+  output = [[0 for x in range(rows1)] for y in range(cols2)]
+  col = 0
+  for row in range(rows1): 
+    new = 0
+    for i in range(cols1): 
+      for j in range(rows2): 
+        new += m1[row][i] * m2[j][col]
+    output[row][col] = new
+    col += 1
+    if(col == cols2-1):
+      col = 0
+  return output
   
 # create an identity matrix of dimensions n-by-n
 def matId(n):
-  # fill-in code here
+  output = [[0 for x in range(n)] for y in range(n)]
+  for i in range(n): 
+    for j in range(n): 
+      if(i == j): 
+        output[i][j] = 1
+  return output
   
 # some sample matricies you can use to test out your functions
 sample1 = [[1,0],[0,1]]
