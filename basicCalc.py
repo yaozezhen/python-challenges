6b188c230552a9a494e1b59541871c50f4827adb
jayala-29 <jayala@ucsd.edu>
2020-06-12 17:15:37 -0700

    Create basicCalc.py

---
+++ basicCalc.py
@@ -0,0 +1,16 @@
+# function descriptor: basicCalc()
+# basic calculator based on user input
+# return type: nothing; program should end when the user types 'n'
+# example: Today we will be finding the different measures a circle gives us.
+#          What would operation would you like to perform? [add|sub|mult] 'add'
+#          The operation you chose was addition.
+#          Enter two numbers to find the sum: '2 5'
+#          Okay, the sum is 7
+#          What would operation would you like to perform? [add|sub|mult] 'sub'
+#          The operation you chose was subtraction.
+#          Enter two numbers to find the difference: '2 5'
+#          Okay, the difference is -3
+#          Do you want to calculate anything else? [y|n] 'n'
+#          Okay, bye bye :)
+def basicCalc():
+  # fill-in code here
