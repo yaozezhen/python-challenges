3498a2e656154631a953a334aa57f0fc955a8e06
yaozezhen <63181901+yaozezhen@users.noreply.github.com>
2020-06-13 01:38:55 -0400

    Update randomEO.py (#42)

--- randomEO.py
+++ randomEO.py
@@ -5,11 +5,13 @@ import urllib.request, json
 # return type: list of 'odd' and 'even' strings
 # example: return ['odd','even','odd','even','even','odd','odd','even','odd','even']
 def randomEO():
-  url = urllib.request.urlopen("https://www.passwordrandom.com/query?command=int&min=0&max=10000&format=json&count=10")
+  n = input("How many random numbers would you like to check? ")
+  web = "https://www.passwordrandom.com/query?command=int&min=0&max=10000&format=json&count=" + n
+  url = urllib.request.urlopen(web)
   data = json.loads(url.read())
   ls = data["random"]
   output = []
-  for i in range(10): 
+  for i in range(int(n)): 
     if (ls[i] % 2 == 0): 
       output.append("even")
     else: 
