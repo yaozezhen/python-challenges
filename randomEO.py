320cfc861889189bbdc61acbcd3fe291f1574a46
jayala-29 <jayala@ucsd.edu>
2020-06-12 20:57:59 -0700

    Update randomEO.py

--- randomEO.py
+++ randomEO.py
@@ -1,6 +1,6 @@
 import urllib.request, json 
 # function descriptor: randomEO()
-# determines if the list of random numbers returned from the URL are odd or even using JSON
+# determines if the list of 10 random numbers returned from the URL are odd or even using JSON 
 # website URL: https://www.passwordrandom.com/query?command=int&min=0&max=10000&format=json&count=10 
 # return type: list of 'odd' and 'even' strings
 # example: return ['odd','even','odd','even','even','odd','odd','even','odd','even']
