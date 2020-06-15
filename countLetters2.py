4cc11df18677fedafbcf51c37c977da69d9139e0
jayala-29 <jayala@ucsd.edu>
2020-06-14 19:41:15 -0700

    Create countLetters2.py

---
+++ countLetters2.py
@@ -0,0 +1,9 @@
+# function descriptor: countLetters2()
+# counts the number of letters the user types when prompted
+# return type: nothing
+# example: Type a string consisting of whatever characters you want: 'fJi298 3_*4th'
+#          There are 5 letters in fJi298 3_*4th
+#          Would you like to type another string? [y|n] 'n'
+#          Okay, bye :)
+def countLetters2():
+  # fill-in code here
