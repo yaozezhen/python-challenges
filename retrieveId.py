f6e7a451e1780ca676e18da6d9b86cdda96990a1
jayala-29 <jayala@ucsd.edu>
2020-06-15 09:45:10 -0700

    Update retrieveId.py

--- retrieveId.py
+++ retrieveId.py
@@ -2,8 +2,8 @@ import urllib.request, json
 # function descriptor: retrieveId(usrname)
 # grabs the id of a user if they exist in the list of members found at https://api.github.com/orgs/treehouses/members
 # usrname - username of a potential member
-# return type: integer; string
-# example: usrname - anikx7 => return 26880012
-#          usrname - anikx8 => return "member does not exist"
+# return type: string
+# example: usrname - anikx7 => return "anikx7 has ID number: 26880012"
+#          usrname - anikx8 => return "anikx8 does not exist in the group"
 def retrieveId(usrname):
   # fill-in code here
