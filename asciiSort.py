ca057217b9c1eb23b964423cd5f935b224f83518
jayala-29 <jayala@ucsd.edu>
2020-06-15 21:00:06 -0700

    Create asciiSort.py

---
+++ asciiSort.py
@@ -0,0 +1,7 @@
+# function descriptor: asciiSort(s)
+# sorts the letters in the string given
+# s - a string that contains characters from the ASCII table
+# return type: string
+# example: s - "k9A* =1>c+" => return "*+19=>Ack "
+def asciiSort(s):
+  # fill-in code here
