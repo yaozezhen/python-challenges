0200b98c245280070dc02505e58041aca03afc2b
yaozezhen <63181901+yaozezhen@users.noreply.github.com>
2020-06-19 01:42:43 -0400

    Update retrieveId.py

--- retrieveId.py
+++ retrieveId.py
@@ -6,4 +6,11 @@ import urllib.request, json
 # example: usrname - anikx7 => return "anikx7 has ID number: 26880012"
 #          usrname - anikx8 => return "anikx8 does not exist in the group"
 def retrieveId(usrname):
-  # fill-in code here
+  url = urllib.request.urlopen("https://api.github.com/orgs/treehouses/members")
+  data = json.loads(url.read())
+  for item in data: 
+    if (item["login"] == usrname): 
+      return usrname + " has ID number: " + str(item["id"])
+  return usrname + " does not exist in the group."
+
+print(retrieveId("275vytran"))
