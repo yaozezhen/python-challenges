5d8427afc28b16a8c26320529efc3eb908cb0268
jayala-29 <jayala@ucsd.edu>
2020-06-19 19:56:59 -0700

    Create advCalc1.py

---
+++ advCalc1.py
@@ -0,0 +1,24 @@
+# function descriptor: advCalc1()
+# advanced calculator that supports addition and subtraction
+# note: input from user does NOT use spaces
+#       part 1 of building PEMDAS-based calculator
+#       this represents parsing an expression as an algorithm
+#       for addition and subtraction, we go "left to right"
+#       the example for should be parsed as follows:
+#           3+7-4+1  <-input
+#           10-4+1
+#           6+1
+#           7        <- output
+# return type: nothing; program should end when the user types 'n'
+# example: Please enter an expression to calculate: '3+7-4+1'
+#          Okay, the output is 7
+#          Do you want to calculate anything else? [y|n] 'n'
+#          Okay, bye bye :)
+def advCalc()
+  # fill-in code here
+  
+def add(a,b):
+  return a + b
+  
+def sub(a,b):
+  return a - b
