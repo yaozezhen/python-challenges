ca05adae33f2ea39b9658bd81c7e9972d21c94c1
yaozezhen <63181901+yaozezhen@users.noreply.github.com>
2020-06-27 02:58:56 -0400

    Update advCalc4.py (#60)
    * Update advCalc4.py
    
    * Update advCalc4.py
    
    Co-authored-by: jayala-29 <jayala@ucsd.edu>

--- advCalc4.py
+++ advCalc4.py
@@ -22,7 +22,102 @@
 #          Okay, bye bye :)
 # you should start by pasting what you have from part 3
 def advCalc4():
-  # fill-in code here
+  while True: 
+    expression = input("Please enter and expression to calculate: ")
+    ls = []
+    numLs = []
+    oprLs = []
+    current = 0
+    lastNum = []
+    pLs = []
+    for item in expression: 
+      ls.append(item)
+    while True: 
+      for i in range(len(ls)): 
+        if(ls[i] == "("): 
+          ls.pop(i)
+          while True: 
+            pLs.append(ls[i])
+            ls.pop(i)
+            if(ls[i] == ")"): 
+              ls.pop(i)
+              break
+          ls.insert(i, paran("".join(pLs)))
+          pLs.clear()
+          break
+      checkParan = 0
+      for i in range(len(ls)): 
+        if(ls[i] == "("): 
+          checkParan += 1
+      if(checkParan == 0): 
+        break
+    for i in range(len(ls)): 
+      if(ls[i] == "+" or ls[i] == "-" or ls[i] == "*" or ls[i] == "/" or ls[i] == "^"): 
+        oprLs.append(ls[i])
+        num = []
+        lastOpr = i
+        for j in range(current,i): 
+          num.append(ls[j])
+        numLs.append("".join(num))
+        current = i+1
+    for i in range(lastOpr+1, len(ls)): 
+      lastNum.append(ls[i])
+    numLs.append("".join(lastNum))
+    while True: 
+      for i in range(len(oprLs)): 
+        if(oprLs[i] == "+" or oprLs[i] == "-" or oprLs[i] == "*" or oprLs[i] == "/"): 
+          continue
+        if(oprLs[i] == "^"): 
+          newNum = expo(int(numLs[i]), int(numLs[i+1]))
+        oprLs.pop(i)
+        numLs.pop(i+1)
+        numLs.pop(i)
+        numLs.insert(i, newNum)
+        break
+      checkExpo = 0
+      for i in range(len(oprLs)): 
+        if(oprLs[i] == "+" or oprLs[i] == "-" or oprLs[i] == "*" or oprLs[i] == "/"): 
+          checkExpo += 1
+      if(checkExpo == len(oprLs)): 
+        break
+    while True: 
+      for i in range(len(oprLs)): 
+        if(oprLs[i] == "+" or oprLs[i] == "-"): 
+          continue
+        if(oprLs[i] == "*"): 
+          newNum = mult(int(numLs[i]), int(numLs[i+1]))
+        if(oprLs[i] == "/"): 
+          newNum = div(int(numLs[i]), int(numLs[i+1]))
+        oprLs.pop(i)
+        numLs.pop(i+1)
+        numLs.pop(i)
+        numLs.insert(i, newNum)
+        break
+      checkMD = 0
+      for i in range(len(oprLs)): 
+        if(oprLs[i] == "+" or oprLs[i] == "-"): 
+          checkMD += 1
+      if(checkMD == len(oprLs)): 
+        break
+    while True: 
+      for i in range(len(oprLs)): 
+        if(oprLs[i] == "+"): 
+          newNum = add(int(numLs[i]), int(numLs[i+1]))
+        if(oprLs[i] == "-"): 
+          newNum = sub(int(numLs[i]), int(numLs[i+1]))
+        oprLs.pop(i)
+        numLs.pop(i+1)
+        numLs.pop(i)
+        numLs.insert(i, newNum)
+        break
+      if(len(oprLs) == 0):
+        break
+    print("Okay, the output is " + str(numLs[0]))
+    more = input("Do you want to calculate anything else? [y|n] ")
+    if(more == "n"): 
+      print("Okay, bye bye :)")
+      break
+  return
   
 def add(a,b):
   return a + b
