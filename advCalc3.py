581d19f5a1018025b7f75a0851ead66e86b17288
yaozezhen <63181901+yaozezhen@users.noreply.github.com>
2020-06-27 00:39:41 -0400

    Update advCalc3.py

--- advCalc3.py
+++ advCalc3.py
@@ -21,7 +21,82 @@
 #          Okay, bye bye :)
 # you should start by pasting what you have from part 2
 def advCalc3():
-  # fill-in code here
+  while True: 
+    expression = input("Please enter and expression to calculate: ")
+    ls = []
+    numLs = []
+    oprLs = []
+    current = 0
+    lastNum = []
+    for item in expression: 
+      ls.append(item)
+    for i in range(len(ls)): 
+      if(ls[i] == "+" or ls[i] == "-" or ls[i] == "*" or ls[i] == "/" or ls[i] == "^"): 
+        oprLs.append(ls[i])
+        num = []
+        lastOpr = i
+        for j in range(current,i): 
+          num.append(ls[j])
+        numLs.append("".join(num))
+        current = i+1
+    for i in range(lastOpr+1, len(ls)): 
+      lastNum.append(ls[i])
+    numLs.append("".join(lastNum))
+    while True: 
+      for i in range(len(oprLs)): 
+        if(oprLs[i] == "+" or oprLs[i] == "-" or oprLs[i] == "*" or oprLs[i] == "/"): 
+          continue
+        if(oprLs[i] == "^"): 
+          newNum = expo(int(numLs[i]), int(numLs[i+1]))
+        oprLs.pop(i)
+        numLs.pop(i+1)
+        numLs.pop(i)
+        numLs.insert(i, newNum)
+        break
+      checkExpo = 0
+      for i in range(len(oprLs)): 
+        if(oprLs[i] == "+" or oprLs[i] == "-" or oprLs[i] == "*" or oprLs[i] == "/"): 
+          checkExpo += 1
+      if(checkExpo == len(oprLs)): 
+        break
+    while True: 
+      for i in range(len(oprLs)): 
+        if(oprLs[i] == "+" or oprLs[i] == "-"): 
+          continue
+        if(oprLs[i] == "*"): 
+          newNum = mult(int(numLs[i]), int(numLs[i+1]))
+        if(oprLs[i] == "/"): 
+          newNum = div(int(numLs[i]), int(numLs[i+1]))
+        oprLs.pop(i)
+        numLs.pop(i+1)
+        numLs.pop(i)
+        numLs.insert(i, newNum)
+        break
+      checkMD = 0
+      for i in range(len(oprLs)): 
+        if(oprLs[i] == "+" or oprLs[i] == "-"): 
+          checkMD += 1
+      if(checkMD == len(oprLs)): 
+        break
+    while True: 
+      for i in range(len(oprLs)): 
+        if(oprLs[i] == "+"): 
+          newNum = add(int(numLs[i]), int(numLs[i+1]))
+        if(oprLs[i] == "-"): 
+          newNum = sub(int(numLs[i]), int(numLs[i+1]))
+        oprLs.pop(i)
+        numLs.pop(i+1)
+        numLs.pop(i)
+        numLs.insert(i, newNum)
+        break
+      if(len(oprLs) == 0):
+        break
+    print("Okay, the output is " + str(numLs[0]))
+    more = input("Do you want to calculate anything else? [y|n] ")
+    if(more == "n"): 
+      print("Okay, bye bye :)")
+      break
+  return
   
 def add(a,b):
   return a + b
@@ -34,6 +109,8 @@ def mult(a,b):
 
 def div(a,b): 
   return a / b
-  
-def exp(a,b):
+
+def expo(a,b): 
   return a ** b
+
+advCalc3()
