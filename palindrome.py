cb573cae5d9c5803d84321b1f146b2bd8f9b5904
jayala-29 <jayala@ucsd.edu>
2020-07-03 19:41:16 -0700

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+# function descriptor: palindrome(s)
+# checks if the string input is a palindrome (same string when reversed)
+# s - string to be checked
+# return type: boolean
+# example: s - "racecar" => return True
+#          s - "abcdca" => return False
+def palindrome(s):
+  # fill-in code here
