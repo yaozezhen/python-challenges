8bbb9d199e1f0f444b64bbaca328f0e2d2dca568
jayala-29 <jayala@ucsd.edu>
2020-07-10 22:15:32 -0700

    Create removeRepeats.py

---
+++ removeRepeats.py
@@ -0,0 +1,7 @@
+# function descriptor: removeRepeats(s)
+# removes repeated characters from a string and returns the result
+# s - a string that contains characters from the ASCII table
+# return type: string
+# example: s - "abc89c ba90* +*" => return "abc89 0*+"
+def removeRepeats(s):
+  # fill-in code here
