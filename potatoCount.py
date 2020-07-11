d57160d370871024c1029d2f527b8d669d6418c3
yaozezhen <63181901+yaozezhen@users.noreply.github.com>
2020-07-11 01:19:19 -0400

    Update potatoCount.py

--- potatoCount.py
+++ potatoCount.py
@@ -4,4 +4,15 @@
 # example: s - "potatopotato" => return 2
 #          s - "potatotomato" => return 1
 def potatoCount(s):
-  # fill-in code here
+  output = 0
+  for i in range(len(s)-5): 
+    if(s[i] == "p"): 
+      if(s[i+1] == "o"): 
+        if(s[i+2] == "t"): 
+          if(s[i+3] == "a"): 
+            if(s[i+4] == "t"): 
+              if(s[i+5] == "o"): 
+                output += 1
+  return output
+
+print(potatoCount("potatotomato"))
