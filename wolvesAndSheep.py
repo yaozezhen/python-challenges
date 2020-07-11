dddab9225ac0c992cd97951a6b69ecbc618d1f5d
yaozezhen <63181901+yaozezhen@users.noreply.github.com>
2020-07-11 02:53:12 -0400

    Update wolvesAndSheep.py (#73)

--- wolvesAndSheep.py
+++ wolvesAndSheep.py
@@ -1,4 +1,17 @@
 def wolvesAndSheep(n,w,s): 
+  b = [[0 for i in range(n)]for j in range(n)]
+  for i in range(n): 
+    l = []
+    for j in range(n): 
+      if([i,j] in w): 
+        b[i][j] = "W"
+      else: 
+        if([i,j] in s): 
+          b[i][j] = "S"
+        else: 
+          b[i][j] = "_"
+      l.append(b[i][j] + " ")
+    print("".join(l))
   board = [[0 for i in range(n)]for j in range(n)]
   for wolf in w: 
     row = wolf[0]
@@ -15,7 +28,7 @@ def wolvesAndSheep(n,w,s):
     while True: 
       r += 1
       c += 1
-      if(r < 4 and c < 4): 
+      if(r < n and c < n): 
         board[r][c] = 1
       else: 
         r = row
@@ -25,7 +38,7 @@ def wolvesAndSheep(n,w,s):
     while True: 
       r += 1
       c -= 1
-      if(r < 4 and c >= 0): 
+      if(r < n and c >= 0): 
         board[r][c] = 1
       else: 
         r = row
@@ -45,20 +58,25 @@ def wolvesAndSheep(n,w,s):
     while True: 
       r -= 1
       c += 1
-      if(r >= 0 and c < 4): 
+      if(r >= 0 and c < n): 
         board[r][c] = 1
       else: 
         break
+  for i in range(n): 
+    l = []
+    for j in range(n): 
+      l.append(str(board[i][j]) + " ")
+    print("".join(l))
+  count = 0
   for sheep in s: 
     if(board[sheep[0]][sheep[1]] == 1): 
-      if len(s) == 1: 
-        return("Your sheep is in DANGER")
-      else: 
-        return("Your sheep are in DANGER")
+      print("Your sheep at row " + str(sheep[0]+1) + ", column " + str(sheep[1]+1) + " is in DANGER.")
+      count += 1
+  if(count == 0): 
+    if len(s) == 1: 
+      print("Your sheep is safe! >o<")
     else: 
-      if len(s) == 1: 
-        return("Your sheep is safe! >o<")
-      else: 
-        return("Your sheep are safe! >o<")
+      print("Your sheep are safe! >o<")
+  return 
 
-print(wolvesAndSheep(4,[[1,0]],[[2,3]]))
+wolvesAndSheep(10,[[1,0],[3,4]],[[2,3],[7,9]])
