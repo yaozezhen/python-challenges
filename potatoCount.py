ac66c97e9c124aca0fab2f1e2f7b2ee44bb8c3fb
jayala-29 <jayala@ucsd.edu>
2020-07-10 21:26:46 -0700

    Create potatoCount.py

---
+++ potatoCount.py
@@ -0,0 +1,7 @@
+# function descriptor: potatoCount(s)
+# create a function to return the amount of potatoes there are in a string
+# return type: integer
+# example: s - "potatopotato" => return 2
+#          s - "potatotomato" => return 1
+def potatoCount(s):
+  # fill-in code here
